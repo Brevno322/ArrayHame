19764d589980f2210242cfc06d11b27680ee70e5
Brevno322 <artyrbrevbo1999@gmail.com>
2022-04-17 10:08:35 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        //Отсортировать числа массива по возрастанию
+        Scanner scanner = new Scanner(System.in);
+//        int ind = scanner.nextInt();
+//        int[] numbers = new int[ind];
+//        for (int i = 0; i < numbers.length; i++) {
+//            numbers[i] = scanner.nextInt();
+//        }
+//        boolean stop = true;
+//        while (stop) {
+//            stop=false;
+//            for (int i = 1; i < numbers.length; i++) {
+//                if (numbers[i] < numbers[i - 1]) {
+//                    int zamena = numbers[i];
+//                    numbers[i] = numbers[i - 1];
+//                    numbers[i - 1] = zamena;
+//
+//                    stop=true;
+//                }
+//
+//
+//                System.out.println(Arrays.toString(numbers));
+//
+//            }
+//        }
+//)Ввести два массива, и создать третий состоящий из элементов первого и второго
+//        int oneArray = scanner.nextInt();
+//        int[] numbersOneArray = new int[oneArray];
+//        for (int i = 0; i < oneArray; i++) {
+//            numbersOneArray[i] = scanner.nextInt();
+//
+//        }
+//        int twoArray = scanner.nextInt();
+//        int[] numbersTwoArray = new int[twoArray];
+//        for (int i = 0; i < twoArray; i++) {
+//            numbersTwoArray[i] = scanner.nextInt();
+//
+//        }
+//        int[] numbersThriArray = new int[oneArray + twoArray];
+//        for (int i = 0; i < oneArray; i++) {
+//            numbersThriArray[i] = numbersOneArray[i];
+//
+//        }
+//        for (int i = oneArray; i <oneArray + twoArray; i++) {
+//            numbersThriArray[i]=numbersTwoArray[twoArray];
+//
+//        }
+//
+//
+//
+//        for (int i = 0; i <oneArray + twoArray; i++) {
+//            System.out.println(Arrays.toString(numbersThriArray));
+//
+//        }
+        //Превратить число в массив из цифр этого числа
+//        String vvod = scanner.next();
+//        int[] numbersArray = new int[vvod.length()];
+//        for (int i = 0; i < vvod.length(); i++) {
+//            numbersArray[i] = vvod.charAt(i) - ('0');
+//            System.out.println(Arrays.toString(numbersArray));
+//        }
+
+    }
+
+}
+
+
